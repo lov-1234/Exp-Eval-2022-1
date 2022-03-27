8442ffc96c173132b2edc1022a2362b5943a217e
Lovnesh <65728534+lov-1234@users.noreply.github.com>
2022-03-27 11:43:48 +0200

    Finished Skeleton

--- SorterHelper.java
+++ SorterHelper.java
@@ -1,34 +1,40 @@
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SorterHelper {
 
-//    Sorter<String> stringSorterPerItem;
-//    Sorter<String> stringSorterUntilNoChange;
-//    Sorter<String> stringSorterWhileNeeded;
-//
-//    Sorter<Integer> integerSorterPerItem;
-//    Sorter<Integer> integerSorterUntilNoChange;
-//    Sorter<Integer> integerSorterWhileNeeded;
-//
-//    Sorter<Double> doubleSorterPerItem;
-//    Sorter<Double> doubleSorterUntilNoChange;
-//    Sorter<Double> doubleSorterWhileNeeded;
+    Sorter<String> stringSorterPerItem;
+    Sorter<String> stringSorterUntilNoChange;
+    Sorter<String> stringSorterWhileNeeded;
+
+    Sorter<Integer> integerSorterPerItem;
+    Sorter<Integer> integerSorterUntilNoChange;
+    Sorter<Integer> integerSorterWhileNeeded;
+
+    Sorter<Double> doubleSorterPerItem;
+    Sorter<Double> doubleSorterUntilNoChange;
+    Sorter<Double> doubleSorterWhileNeeded;
+    Random randomGenerator;
+
+    public static int ITERATIONS  = 500;
+    public static int WARMUP = 100;
+
 
     public SorterHelper() {
         // Pass
-//        stringSorterPerItem = new BubbleSortPassPerItem<>();
-//        stringSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-//        stringSorterWhileNeeded = new BubbleSortWhileNeeded<>();
-//
-//        integerSorterPerItem = new BubbleSortPassPerItem<>();
-//        integerSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-//        integerSorterWhileNeeded = new BubbleSortWhileNeeded<>();
-//
-//        doubleSorterPerItem = new BubbleSortPassPerItem<>();
-//        doubleSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-//        doubleSorterWhileNeeded = new BubbleSortWhileNeeded<>();
+        randomGenerator = new Random();
+        stringSorterPerItem = new BubbleSortPassPerItem<>();
+        stringSorterUntilNoChange = new BubbleSortUntilNoChange<>();
+        stringSorterWhileNeeded = new BubbleSortWhileNeeded<>();
+
+        integerSorterPerItem = new BubbleSortPassPerItem<>();
+        integerSorterUntilNoChange = new BubbleSortUntilNoChange<>();
+        integerSorterWhileNeeded = new BubbleSortWhileNeeded<>();
+
+        doubleSorterPerItem = new BubbleSortPassPerItem<>();
+        doubleSorterUntilNoChange = new BubbleSortUntilNoChange<>();
+        doubleSorterWhileNeeded = new BubbleSortWhileNeeded<>();
     }
 
     public String getAlphaNumericString(int n) // https://www.geeksforgeeks.org/generate-random-string-of-given-size-in-java/
@@ -73,15 +79,15 @@ public class SorterHelper {
         return listToStringArray(stringArray);
     }
 
-    public String[] returnStirngArrayCopy(String[] stringArray) {
+    public String[] returnStringArrayCopy(String[] stringArray) {
         var s = new String[stringArray.length];
         System.arraycopy(stringArray, 0, s, 0, stringArray.length);
         return s;
     }
 
-    public Double[] returnDoubleArrayCopy(Double[] longArray) {
-        var s = new Double[longArray.length];
-        System.arraycopy(longArray, 0, s, 0, longArray.length);
+    public Double[] returnDoubleArrayCopy(Double[] doubleArray) {
+        var s = new Double[doubleArray.length];
+        System.arraycopy(doubleArray, 0, s, 0, doubleArray.length);
         return s;
     }
 
@@ -91,40 +97,208 @@ public class SorterHelper {
         return s;
     }
 
-    public List<Long> checkStringArrayPerformance(int iterations, Sorter<String> sorter, String[] stringArray) {
+    public Long checkStringArrayPerformance(int iterations, Sorter<String> sorter, String[] stringArray) {
         var timeList = new ArrayList<Long>();
         for (int i = 0; i < iterations; i++) {
-            var copy = returnStirngArrayCopy(stringArray);
+            var copy = returnStringArrayCopy(stringArray);
             var startTime = System.nanoTime();
             sorter.sort(copy);
             var endTime = System.nanoTime();
-            timeList.add(endTime - startTime);
+            if (i > WARMUP)
+                timeList.add(endTime - startTime);
         }
-        return timeList;
+        return timeList.stream().reduce((long)0, (i, j)-> (i + j)/iterations);
     }
 
-    public List<Long> checkDoubleArrayPerformance(int iterations, Sorter<Double> sorter, Double[] longArray) {
+    public Long checkDoubleArrayPerformance(int iterations, Sorter<Double> sorter, Double[] doubleArray) {
         var timeList = new ArrayList<Long>();
         for (int i = 0; i < iterations; i++) {
-            var copy = returnDoubleArrayCopy(longArray);
+            var copy = returnDoubleArrayCopy(doubleArray);
             var startTime = System.nanoTime();
             sorter.sort(copy);
             var endTime = System.nanoTime();
-            timeList.add(endTime - startTime);
+            if (i > WARMUP)
+                timeList.add(endTime - startTime);
         }
-        return timeList;
+        return timeList.stream().reduce((long)0, (i, j)-> (i + j)/iterations);
     }
 
-    public List<Long> checkIntegerArrayPerformance(int iterations, Sorter<Integer> sorter, Integer[] integerArray) {
+    public Long checkIntegerArrayPerformance(int iterations, Sorter<Integer> sorter, Integer[] integerArray) {
         var timeList = new ArrayList<Long>();
         for (int i = 0; i < iterations; i++) {
             var copy = returnIntegerArrayCopy(integerArray);
             var startTime = System.nanoTime();
             sorter.sort(copy);
             var endTime = System.nanoTime();
-            timeList.add(endTime - startTime);
+            if (i > WARMUP)
+                timeList.add(endTime - startTime);
         }
-        return timeList;
+        return timeList.stream().reduce((long)0, (i, j)-> (i + j)/iterations);
     }
 
+    public Double[] worstCaseDoubleList(int size) {
+        ArrayList<Double> doubleArray = randomGenerator.doubles(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Double[doubleArray.size()];
+        doubleArray.sort(Collections.reverseOrder());
+        s = doubleArray.toArray(s);
+        return s;
+    }
+
+    public Double[] bestCaseDoubleList(int size) {
+        ArrayList<Double> doubleArray = randomGenerator.doubles(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Double[doubleArray.size()];
+        Collections.sort(doubleArray);
+        s = doubleArray.toArray(s);
+        return s;
+    }
+
+    public Integer[] worstCaseIntegerList(int size) {
+        ArrayList<Integer> integerArray = randomGenerator.ints(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Integer[integerArray.size()];
+        integerArray.sort(Collections.reverseOrder());
+        s = integerArray.toArray(s);
+        return s;
+    }
+
+    public Integer[] bestCaseIntegerList(int size) {
+        ArrayList<Integer> integerArray = randomGenerator.ints(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Integer[integerArray.size()];
+        Collections.sort(integerArray);
+        s = integerArray.toArray(s);
+        return s;
+    }
+
+    public Double[] randomCaseDoubleList(int size) {
+        ArrayList<Double> doubleArray = randomGenerator.doubles(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Double[doubleArray.size()];
+        s = doubleArray.toArray(s);
+        return s;
+    }
+
+    public Integer[] randomCaseIntegerList(int size) {
+        ArrayList<Integer> integerArray = randomGenerator.ints(size).boxed().collect(Collectors.toCollection(ArrayList::new));
+        var s = new Integer[integerArray.size()];
+        s = integerArray.toArray(s);
+        return s;
+    }
+
+    public Performance checkStringRandomCasePerformanceWithAllSorters(int size, int stringSize){
+        var stringArrayOne = getStringArrayOfDesiredLength(stringSize, size);
+        var performancePerItem = checkStringArrayPerformance(ITERATIONS, stringSorterPerItem, stringArrayOne);
+        var performanceUntilNoChange = checkStringArrayPerformance(ITERATIONS, stringSorterUntilNoChange, stringArrayOne);
+        var performanceWhileNeeded = checkStringArrayPerformance(ITERATIONS, stringSorterWhileNeeded, stringArrayOne);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.STRING);
+    }
+
+    public Performance checkStringWorstCasePerformanceWithAllSorters(int size, int stringSize){
+        var stringArrayOne = getStringArrayOfDesiredLength(stringSize, size);
+        var a = new ArrayList<>(Arrays.asList(stringArrayOne));
+        a.sort(Collections.reverseOrder());
+        stringArrayOne = a.toArray(stringArrayOne);
+        var performancePerItem = checkStringArrayPerformance(ITERATIONS, stringSorterPerItem, stringArrayOne);
+        var performanceUntilNoChange = checkStringArrayPerformance(ITERATIONS, stringSorterUntilNoChange, stringArrayOne);
+        var performanceWhileNeeded = checkStringArrayPerformance(ITERATIONS, stringSorterWhileNeeded, stringArrayOne);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.STRING);
+    }
+
+    public Performance checkStringBestCasePerformanceWithAllSorters(int size, int stringSize){
+        var stringArrayOne = getStringArrayOfDesiredLength(stringSize, size);
+        var a = new ArrayList<>(Arrays.asList(stringArrayOne));
+        Collections.sort(a);
+        stringArrayOne = a.toArray(stringArrayOne);
+        var performancePerItem = checkStringArrayPerformance(ITERATIONS, stringSorterPerItem, stringArrayOne);
+        var performanceUntilNoChange = checkStringArrayPerformance(ITERATIONS, stringSorterUntilNoChange, stringArrayOne);
+        var performanceWhileNeeded = checkStringArrayPerformance(ITERATIONS, stringSorterWhileNeeded, stringArrayOne);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.STRING);
+    }
+
+
+    public Performance checkIntegerWorstCasePerformance(int arraySize) {
+        var integerArray = worstCaseIntegerList(arraySize);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, integerArray);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, integerArray);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, integerArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.INTEGER);
+    }
+
+    public Performance checkIntegerBestCasePerformance(int arraySize) {
+        var integerArray = bestCaseIntegerList(arraySize);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, integerArray);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, integerArray);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, integerArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.INTEGER);
+    }
+
+    public Performance checkIntegerRandomCasePerformance(int arraySize) {
+        var integerArray = randomCaseIntegerList(arraySize);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, integerArray);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, integerArray);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, integerArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.INTEGER);
+    }
+
+    public Performance checkDoubleWorstCasePerformance(int arraySize) {
+        var doubleArray = worstCaseDoubleList(arraySize);
+        var performancePerItem = checkDoubleArrayPerformance(ITERATIONS, doubleSorterPerItem, doubleArray);
+        var performanceUntilNoChange = checkDoubleArrayPerformance(ITERATIONS, doubleSorterUntilNoChange, doubleArray);
+        var performanceWhileNeeded = checkDoubleArrayPerformance(ITERATIONS, doubleSorterWhileNeeded, doubleArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.DOUBLE);
+    }
+
+    public Performance checkDoubleBestCasePerformance(int arraySize) {
+        var doubleArray = bestCaseDoubleList(arraySize);
+        var performancePerItem = checkDoubleArrayPerformance(ITERATIONS, doubleSorterPerItem, doubleArray);
+        var performanceUntilNoChange = checkDoubleArrayPerformance(ITERATIONS, doubleSorterUntilNoChange, doubleArray);
+        var performanceWhileNeeded = checkDoubleArrayPerformance(ITERATIONS, doubleSorterWhileNeeded, doubleArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.DOUBLE);
+    }
+
+    public Performance checkDoubleRandomCasePerformance(int arraySize) {
+        var doubleArray = randomCaseDoubleList(arraySize);
+        var performancePerItem = checkDoubleArrayPerformance(ITERATIONS, doubleSorterPerItem, doubleArray);
+        var performanceUntilNoChange = checkDoubleArrayPerformance(ITERATIONS, doubleSorterUntilNoChange, doubleArray);
+        var performanceWhileNeeded = checkDoubleArrayPerformance(ITERATIONS, doubleSorterWhileNeeded, doubleArray);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, arraySize, Type.DOUBLE);
+    }
+
+    public Performance checkRandomIntegerSortOnSmallerInputs(int size) {
+        var intArray = new ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            intArray.add(i);
+        }
+        Collections.shuffle(intArray);
+        var s = new Integer[intArray.size()];
+        s = intArray.toArray(s);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, s);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, s);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, s);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.INTEGER);
+    }
+
+    public Performance checkBestIntegerSortOnSmallerInputs(int size) {
+        var intArray = new ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            intArray.add(i);
+        }
+        var s = new Integer[intArray.size()];
+        s = intArray.toArray(s);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, s);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, s);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, s);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.INTEGER);
+    }
+
+    public Performance checkWorstIntegerSortOnSmallerInputs(int size) {
+        var intArray = new ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            intArray.add(i);
+        }
+        intArray.sort(Collections.reverseOrder());
+        var s = new Integer[intArray.size()];
+        s = intArray.toArray(s);
+        var performancePerItem = checkIntegerArrayPerformance(ITERATIONS, integerSorterPerItem, s);
+        var performanceUntilNoChange = checkIntegerArrayPerformance(ITERATIONS, integerSorterUntilNoChange, s);
+        var performanceWhileNeeded = checkIntegerArrayPerformance(ITERATIONS, integerSorterWhileNeeded, s);
+        return new Performance(performancePerItem, performanceUntilNoChange, performanceWhileNeeded, size, Type.INTEGER);
+    }
 }
--- SorterTester.java
+++ SorterTester.java
@@ -5,77 +5,15 @@ import java.util.stream.Collectors;
 public class SorterTester {
 
 
-    private static final int SIZE_OF_ARRAY = 10;
-    private static final Random randomGenerator = new Random();
     private static final int TEN = 10;
     private static final int HUNDRED = 100;
     private static final int THOUSAND = 1000;
-
+    public static int SIZE_ONE = 10;
+    public static int SIZE_TWO = 100;
+    public static int SIZE_THREE = 1000;
 
     public static void main(String[] args) {
         var sorterHelper = new SorterHelper();
-        Sorter<String> stringSorterPerItem = new BubbleSortPassPerItem<>();
-        Sorter<String> stringSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-        Sorter<String> stringSorterWhileNeeded = new BubbleSortWhileNeeded<>();
-
-        Sorter<Integer> integerSorterPerItem = new BubbleSortPassPerItem<>();
-        Sorter<Integer> integerSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-        Sorter<Integer> integerSorterWhileNeeded = new BubbleSortWhileNeeded<>();
-
-        Sorter<Double> doubleSorterPerItem = new BubbleSortPassPerItem<>();
-        Sorter<Double> doubleSorterUntilNoChange = new BubbleSortUntilNoChange<>();
-        Sorter<Double> doubleSorterWhileNeeded = new BubbleSortWhileNeeded<>();
-
-//        ArrayList<Double> doubleArray = randomGenerator.doubles(SIZE_OF_ARRAY).boxed().collect(Collectors.toCollection(ArrayList::new));
-//
-//        ArrayList<Integer> intArray = randomGenerator.ints(SIZE_OF_ARRAY).boxed().collect(Collectors.toCollection(ArrayList::new));
-
-        var stringArrayOne = sorterHelper.getStringArrayOfDesiredLength(TEN, SIZE_OF_ARRAY);
-
-        var performance = sorterHelper.checkStringArrayPerformance(100, stringSorterPerItem, stringArrayOne);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterUntilNoChange, stringArrayOne);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterWhileNeeded, stringArrayOne);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-        var stringArrayTwo = sorterHelper.getStringArrayOfDesiredLength(HUNDRED, SIZE_OF_ARRAY);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterPerItem, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterUntilNoChange, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterWhileNeeded, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-
-
-        var stringArrayThree = sorterHelper.getStringArrayOfDesiredLength(THOUSAND, SIZE_OF_ARRAY);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterPerItem, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterUntilNoChange, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-        performance = sorterHelper.checkStringArrayPerformance(100, stringSorterWhileNeeded, stringArrayTwo);
-        System.out.println(performance.stream().reduce((long)0, (i, j)-> i + j)/100);
-
-//
-//
-//        var stringArrayLargeOne = sorterHelper.getStringArrayOfDesiredLength(TEN, Math.pow(SIZE_OF_ARRAY, SIZE_OF_ARRAY));
-//
-//
-//        var stringArrayLargeTwo = sorterHelper.getStringArrayOfDesiredLength(HUNDRED, Math.pow(SIZE_OF_ARRAY, SIZE_OF_ARRAY));
-//
-//
-//        var stringArrayLargeThree = sorterHelper.getStringArrayOfDesiredLength(THOUSAND, Math.pow(SIZE_OF_ARRAY, SIZE_OF_ARRAY));
-
-
-
 
     }
 }
